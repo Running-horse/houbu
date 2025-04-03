b25c776487372b695a6e79f9bb8b4d2280fa8d7c
Running-horse <114240816+Running-horse@users.noreply.github.com>
2025-04-03 08:23:35 +0800

    Update train.py

--- train.py
+++ train.py
@@ -3,12 +3,12 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('ultralytics/cfg/models/11/yolo11-CGAFusion-MSAM.yaml')
+    model = YOLO('ultralytics/cfg/models/11/yolo11.yaml')
     #model.load('best.pt') # loading pretrain weights
     model.train(data='data/Rail.yaml',
                 cache=False,
                 imgsz=640,
-                epochs=300,
+                epochs=100,
                 batch=16,
                 workers=0,
                 device='0',
